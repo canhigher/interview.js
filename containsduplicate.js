0ea16c4c1ce4ab9e1058969a03b4e426ccbac94d
canhigher <wrcyksl@gmail.com>
2021-04-23 23:14:00 +0300

    added test of the function

--- containsduplicate.js
+++ containsduplicate.js
@@ -2,3 +2,6 @@ var containsDuplicate = function(nums) {
     let mySet = new Set(nums);
     return (mySet.size != nums.length)
 };
+
+let res = containsDuplicate([1,1,1,3,3,4,3,2,4,2]);
+console.log(res);
