b279a261007de2b48562f791bc10f4321e2a00be
canhigher <wrcyksl@gmail.com>
2021-04-30 21:52:28 +0300

    BST implementation

---
+++ data_structures/BinarySearchTree.js
@@ -0,0 +1,73 @@
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+    this.size = 0;
+  }
+  push(value) {
+    let node = new Node(value);
+    let current = this.root;
+    if (!this.root) {
+      this.root = node;
+      this.size++;
+      return this;
+    }
+    while (true) {
+      if (value >= current.data) {
+        if (current.right) {
+          current = current.right;
+        } else {
+          current.right = node;
+          this.size++;
+          return this;
+        }
+      } else {
+        if (current.left) {
+          current = current.left;
+        } else {
+          current.left = node;
+          this.size++;
+          return this;
+        }
+      }
+    }
+  }
+  find(value) {
+    if (!this.root) {
+      return false;
+    }
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value == current.data) {
+        found = true;
+      } else if (value > current.data) {
+        current = current.right;
+      } else {
+        current = current.left;
+      }
+    }
+    return found;
+  }
+  _size() {
+    return this.size;
+  }
+}
+
+let myTree = new BinarySearchTree();
+myTree.push(10);
+myTree.push(5);
+myTree.push(8);
+myTree.push(13);
+myTree.push(9);
+let found = myTree.find(9);
+console.log("found: ", found);
+let size = myTree._size();
+console.log("size", size);
