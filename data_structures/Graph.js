3fcc87ab04a8dfe3bf23e0837a8a0284a86c2bb4
canhigher <wrcyksl@gmail.com>
2021-05-03 22:37:45 +0300

    graph class functions

--- data_structures/Graph.js
+++ data_structures/Graph.js
@@ -3,6 +3,36 @@ class Graph {
     this.adjacencyList = {};
   }
   addVertex(vertex) {
-    this.adjencyList[vertex] = [];
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+    return this;
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v != vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v != vertex1
+    );
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const popped = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, popped);
+    }
+    delete this.adjacencyList[vertex];
   }
 }
+
+let g = new Graph();
+
+let f = g.addVertex("Istanbul");
+let s = g.addVertex("Lisbon");
+let t = g.addEdge("Istanbul", "Lisbon");
+
+console.log("f", f);
+console.log("s", s);
+console.log("g", g);
